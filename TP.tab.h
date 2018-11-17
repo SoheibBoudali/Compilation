b131a6d5ac162b60efa54938ad79c98a73a88b29
SoheibBoudali <39391107+SoheibBoudali@users.noreply.github.com>
2018-11-18 00:50:27 +0100

    Annalyse syntaxique terminée

---
+++ TP.tab.h
@@ -0,0 +1,81 @@
+/* A Bison parser, made by GNU Bison 2.1.  */
+
+/* Skeleton parser for Yacc-like parsing with Bison,
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, when this file is copied by Bison into a
+   Bison output file, you may use that output file without restriction.
+   This special exception was added by the Free Software Foundation
+   in version 1.24 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     Programme = 258,
+     While = 259,
+     EXECUT = 260,
+     IF = 261,
+     Real = 262,
+     Integer = 263,
+     BOUCLE = 264,
+     TAB = 265,
+     Calcul = 266,
+     CONST = 267,
+     EGAL = 268,
+     SEP = 269,
+     IDF = 270,
+     OPR = 271,
+     ENTIER = 272,
+     REEL = 273
+   };
+#endif
+/* Tokens.  */
+#define Programme 258
+#define While 259
+#define EXECUT 260
+#define IF 261
+#define Real 262
+#define Integer 263
+#define BOUCLE 264
+#define TAB 265
+#define Calcul 266
+#define CONST 267
+#define EGAL 268
+#define SEP 269
+#define IDF 270
+#define OPR 271
+#define ENTIER 272
+#define REEL 273
+
+
+
+
+#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
+
+
---
+++ TP.y
@@ -0,0 +1,104 @@
+%{
+#include<stdio.h>
+//#include"main.c"
+extern FILE* yyin;
+extern int nbl,nbc;
+%}
+/*%union
+{
+char *chaine;
+int entier;
+float reel;
+}*/
+%token '{' '}' '(' ')' ':' ';' ',' '+' '*' '-' '/' '[' ']' 
+%token Programme While EXECUT IF Real Integer BOUCLE TAB Calcul CONST EGAL SEP 
+%token <chaine> IDF
+%token <chaine> OPR
+%token <entier> ENTIER
+%token <reel> REEL
+%%
+
+Start : Bibliotheque Programme IDF '{' DECLARATION  INSTRUCTION '}' {printf("programme correct syntaxiquement\n");} ;
+
+Bibliotheque : Bib Bibliotheque 
+            |  Bib 
+;
+
+Bib : Calcul 
+     | TAB   
+     | BOUCLE 
+;
+
+DECLARATION : TYPE Val DECLARATION
+  | TYPE Val
+;
+
+Val : DecVar
+  | DecTab
+  | CONST DecConst
+;
+
+DecVar: IDF SEP DecVar      
+       | IDF ';'       
+;
+
+DecConst: IDF SEP DecConst  
+        | IDF ';'      
+;
+
+DecTab: IDF '[' ENTIER ']' ';'     
+;
+
+TYPE: Real  
+      | Integer
+;
+
+INSTRUCTION: TypeInstruction INSTRUCTION
+    |TypeInstruction
+;
+
+TypeInstruction: LOOP
+                | CONDITION
+                | AFFECTATION
+;
+
+AFFECTATION : IDFA EGAL EXP ';' 
+;
+
+EXP : EXP '+' EXPPRIO 
+     | EXP '-' EXPPRIO 
+     | EXPPRIO 
+;
+
+EXPPRIO :  EXPPRIO '*' AFF 
+     | EXPPRIO '/' AFF 
+     | AFF
+;
+
+AFF : IDF 
+     | IDF '['ENTIER']'  
+     | ENTIER 
+     | REEL  
+;
+
+IDFA: IDF 
+        | IDF '['ENTIER']'  
+;
+
+LOOP: While '(' EXP OPR EXP ')' '{' INSTRUCTION '}'
+;
+
+CONDITION: EXECUT INSTRUCTION IF '(' EXP OPR EXP ')' 
+;
+%%
+int yyerror(char* msg)
+{
+printf("Erreur syntaxique a la ligne %d colonne %d\n",nbl,nbc);
+return 1;
+}
+int main()
+{
+yyin=fopen("Test.txt","r");
+yyparse();
+return 0;
+}
